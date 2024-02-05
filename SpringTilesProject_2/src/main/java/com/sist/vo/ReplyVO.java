b3520595d569134093d5ef13e27034cebbcf8536
SunhoKim <92847080+DevsunhoKim@users.noreply.github.com>
2024-02-05 17:54:18 +0900

    Spring tiles

---
+++ SpringTilesProject_2/src/main/java/com/sist/vo/ReplyVO.java
@@ -0,0 +1,10 @@
+package com.sist.vo;
+import java.util.*;
+
+import lombok.Data;
+@Data
+public class ReplyVO {
+ private int fno,rno;
+ private String id,msg,dbday,name;
+ private Date regdate;
+}
